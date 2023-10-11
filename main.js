438a76a013bc63c169474368a1e5a52f12d5063c
adamnear <nearing14@hotmail.com>
2023-10-11 16:14:12 -0300

    for loop for full house

--- main.js
+++ main.js
@@ -1,127 +1,179 @@
-const apiUrl = 'https://deckofcardsapi.com/api/deck';
-let deckId = localStorage.getItem('deckId');
+(function () {
+    const apiUrl = 'https://deckofcardsapi.com/api/deck';
+    let deckId = localStorage.getItem('deckId');
 
-//Async Fetch the cards from the API and store as cardData
-async function retrieveDeck() {
+    //Async Fetch the cards from the API and store as cardData
+    async function retrieveDeck() {
 
-    const response = await fetch(`${apiUrl}/new`);
-    const cardData = await response.json();
-    deckId = cardData.deck_id;
+        const response = await fetch(`${apiUrl}/new`);
+        const cardData = await response.json();
+        deckId = cardData.deck_id;
 
-    //Store the deckId in localStorage using .setItem
-    localStorage.setItem('deckId', deckId);
+        //Store the deckId in localStorage using .setItem
+        localStorage.setItem('deckId', deckId);
 
-    //Show the deck id in the console.
-    console.log("Retrieved deck_id: ", deckId);
-};
-//Call the retrieveDeck function
-retrieveDeck();
+        //Show the deck id in the console.
+        console.log("Retrieved deck_id: ", deckId);
+    };
+    //Call the retrieveDeck function
+    retrieveDeck();
 
-//Async function to draw five cards from the deck api
-async function shuffleAndDrawCards(deckId) {
+    //Async function to draw five cards from the deck api
+    async function shuffleAndDrawCards(deckId) {
 
-    //Shuffle the deck from the apiUrl before drawing
-    const shuffleUrl = `${apiUrl}/${deckId}/shuffle/`;
-    const shuffleResponse = await fetch(shuffleUrl);
+        //Shuffle the deck from the apiUrl before drawing
+        const shuffleUrl = `${apiUrl}/${deckId}/shuffle/`;
+        const shuffleResponse = await fetch(shuffleUrl);
 
-    //Drawing 5 cards from the apiUrl with the associated deckId
-    const drawUrl = `${apiUrl}/${deckId}/draw/?count=5`;
-    const drawResponse = await fetch(drawUrl);
-    const handData = await drawResponse.json();
-    const hand = handData.cards;
+        //Drawing 5 cards from the apiUrl with the associated deckId
+        const drawUrl = `${apiUrl}/${deckId}/draw/?count=5`;
+        const drawResponse = await fetch(drawUrl);
+        const handData = await drawResponse.json();
+        const hand = handData.cards;
 
-    //Display image of the cards delt
-    const cardImagesContainer = document.getElementById("cardImages");
-    const outputCardDetails = document.getElementById("cardDetails");
+        //Display image of the cards delt
+        const cardImagesContainer = document.getElementById("cardImages");
+        const outputCardDetails = document.getElementById("cardDetails");
 
-    cardImagesContainer.innerHTML = ""; //Clearing the previous card images displayed
-    let outputHTML = "";
+        cardImagesContainer.innerHTML = ""; //Clearing the previous card images displayed
+        let outputHTML = "";
 
-    hand.forEach(card => {
-        const cardImage = document.createElement("img");
-        outputHTML += `<h3>${card.value} of ${card.suit}</h3>`;
-        cardImage.src = card.image;
-        cardImagesContainer.appendChild(cardImage);
+        hand.forEach(card => {
+            const cardImage = document.createElement("img");
+            outputHTML += `<h3>${card.value} of ${card.suit}</h3>`;
+            cardImage.src = card.image;
+            cardImagesContainer.appendChild(cardImage);
+        });
+
+        outputCardDetails.innerHTML = outputHTML;//The card number and value sent to HTML
+
+        //Show the cards delt in the console
+        console.log("Hand delt: ", hand);
+    };
+    //Event listener for shuffle and deal button
+    document.getElementById('shuffleAndDeal').addEventListener('click', () => {
+        //Call drawCards function after button is clicked
+        shuffleAndDrawCards(deckId);
     });
 
-    outputCardDetails.innerHTML = outputHTML;//The card number and value sent to HTML
 
-    //Show the cards delt in the console
-    console.log("Hand delt: ", hand);
-};
-//Event listener for shuffle and deal button
-document.getElementById('shuffleAndDeal').addEventListener('click', () => {
-    //Call drawCards function after button is clicked
-    shuffleAndDrawCards(deckId);
-});
+    //Determining all possible poker hands
+    function determinePokerHand(cards) {
+        //Organize the cards to be in rank from high to low
+        const sortedCards = cards.slice();
+        sortedCards.sort((a, b) => {
+            const rankA = getCardRankValue(a);
+            const rankB = getCardRankValue(b);
+            return rankB - rankA;
+        });
 
-
-//Determining all possible poker hands
-function determinePokerHand(cards) {
-    //Organize the cards to be in rank from high to low
-    cards.sort((a, b) => {
-        const rankA = getCardRankValue(a);
-        const rankB = getCardRankValue(b);
-        return rankB - rankA;
-    });
-
-    //These are the possible poker hands, from highest value to lowest value
-    if (isRoyalFlush(cards)) {
-        return "Royal Flush";
-    } else if (isStraightFlush(cards)) {
-        return "Straight Flush";
-    } else if (isFourOfAKind(cards)) {
-        return "Four of a Kind";
-    } else if (isFullHouse(cards)) {
-        return "Full House";
-    } else if (isFlush(cards)) {
-        return "Flush";
-    } else if (isStraight(cards)) {
-        return "Straight";
-    } else if (isThreeOfAKind(cards)) {
-        return "Three of a Kind";
-    } else if (isTwoPair(cards)) {
-        return "Two Pair";
-    } else if (isOnePair(cards)) {
-        return "One Pair";
-    } else {
-        return "High Card";
+        //These are the possible poker hands, from highest value to lowest value
+        if (isRoyalFlush(sortedCards)) {
+            return "Royal Flush";
+        } else if (isStraightFlush(sortedCards)) {
+            return "Straight Flush";
+        } else if (isFourOfAKind(sortedCards)) {
+            return "Four of a Kind";
+        } else if (isFullHouse(sortedCards)) {
+            return "Full House";
+        } else if (isFlush(sortedCards)) {
+            return "Flush";
+        } else if (isStraight(sortedCards)) {
+            return "Straight";
+        } else if (isThreeOfAKind(sortedCards)) {
+            return "Three of a Kind";
+        } else if (isTwoPair(sortedCards)) {
+            return "Two Pair";
+        } else if (isOnePair(sortedCards)) {
+            return "One Pair";
+        } else {
+            return "High Card";
+        }
     }
-}
 
-//function to assign value to non-numeric cards (face cards)
-function getCardRankValue(card) {
-    const rank = card.value;
-}
+    //function to assign value to non-numeric cards (face cards)
+    function getCardRankValue(sortedCards) {
+        const rank = sortedCards.value;
+        return isNaN(rank) ? "TJQKA".indexOf(rank) + 10 : parseInt(rank); //checking if not a number = true, then it is a face card or 10 
+        //"TJQKA" is an ordered list starting at T = 0. Then adding 10 so T = 10
+    }
 
-function isRoyalFlush(cards) {
-    //Check for a royal flush
-}
+    function isRoyalFlush(sortedCards) {
+        //Check for a royal flush
+        const validCards = ["T", "J", "Q", "K", "A"];
+        let hasValidCards = true;
+        let firstSuit = null;
 
-function isStraightFlush(cards) {
-    //Check for a straight flush
-}
+        for (const card of sortedCards) { //Loop through each card in the cards array
+            if (validCards.includes(card.charAt(0))) { //Checking if the card rank is in validCards
+                if (firstSuit === null) { //firstSuit will be the standard suit
+                    firstSuit = card.charAt(1); //Taking the second character (the suit) of the card string 
+                } else if (firstSuit !== card.charAt(1)) { //firstSuit is not equal to current card
+                    hasValidCards = false; //at least one card is out of suit
+                    break;
+                }
+            } else {
+                hasValidCards = false; //Card rank is not in validCards
+                break;
+            }
+        }
+        return hasValidCards && firstSuit != null;//hasValidCards is the valid ranks; firstSuit is the same suit
+    }
 
-function isFourOfAKind(cards) {
-    //Check for four of a kind
-}
+    function isStraightFlush(sortedCards) {
+        //Check for a straight flush
+        const firstSuit = sortedCards[0].suit; //Card at index 0 will carry the suit standard (firstSuit)
 
-function isFullHouse(cards) {
-    //Check for a full house
-}
+        if (sortedCards.every(card => card.suit === firstSuit)) {//checking that all cards follow same suit as the firstSuit
+            for (let i = 1; i < sortedCards.length; i++) { //Start at second card since we have firstSuit
+                if (getCardRankValue(sortedCards[i - 1]) !== getCardRankValue(sortedCards[i] + 1)) {
+                    // Check if the rank of the current card is not one less than the rank of the previous card
+                    return false; //if false, it's not a straight with same suit
+                }
+            }
+            return true
+        }
+        return false;
+    }
 
-function isFlush(cards) {
-    //Check for a flush
-}
+    function isFourOfAKind(sortedCards) {
+        //Check for four of a kind
+        const rankCounts = {} //object to count the frequency of each rank
 
-function isStraight(cards) {
-    //Check for a Stright
-}
+        for (const card of sortedCards) {
+            const rank = card.value;
 
-function isTwoPair(cards) {
-    //Check for a two pair
-}
+            rankCounts[rank] = (rankCounts[rank] || 0) + 1;
 
-function isOnePair(cards) {
-    //Check for a one pair
-}
+            if (rankCounts[rank] === 4) {
+                return true //Checking if the same rank (value) appears 4 times
+            }
+        }
+        return false;
+    }
+
+    function isFullHouse(sortedCards) {
+        //Check for a full house
+        const rankCounts = {} //object to count the frequency of each rank
+
+        for (const card of sortedCards) {
+            const rank = card.value;
+        }
+    }
+
+    function isFlush(sortedCards) {
+        //Check for a flush
+    }
+
+    function isStraight(sortedCards) {
+        //Check for a Stright
+    }
+
+    function isTwoPair(sortedCards) {
+        //Check for a two pair
+    }
+
+    function isOnePair(sortedCards) {
+        //Check for a one pair
+    }
+})();
